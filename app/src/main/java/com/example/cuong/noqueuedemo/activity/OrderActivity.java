60f0f82c61735afc6796d4a41beace457cf2fcff
Tô Quốc Cường <cuongtqse62877@fpt.edu.vn>
2018-10-10 11:52:41 +0700

    [NoQueue] format code

--- app/src/main/java/com/example/cuong/noqueuedemo/activity/OrderActivity.java
+++ app/src/main/java/com/example/cuong/noqueuedemo/activity/OrderActivity.java
@@ -1,11 +1,15 @@
 package com.example.cuong.noqueuedemo.activity;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
@@ -17,7 +21,7 @@ import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
 
 public class OrderActivity extends AppCompatActivity implements View.OnClickListener{
     private FragmentPagerItemAdapter mAdapter;
-    private ImageView mImgBack;
+    private ImageView mImgBack, mImgRefresh;
     private LinearLayout mLnlCheckOut;
 
 
@@ -34,6 +38,8 @@ public class OrderActivity extends AppCompatActivity implements View.OnClickList
         mImgBack.setOnClickListener(this);
         mLnlCheckOut = findViewById(R.id.linear_layout_checkout_cart);
         mLnlCheckOut.setOnClickListener(this);
+        mImgRefresh = findViewById(R.id.image_view_refresh);
+        mImgRefresh.setOnClickListener(this);
     }
 
     private void initialData() {
@@ -70,6 +76,32 @@ public class OrderActivity extends AppCompatActivity implements View.OnClickList
             case R.id.image_view_icon_back:
                 finish();
                 break;
+            case R.id.image_view_refresh:
+
+                break;
+
         }
     }
+
+    private void showRefreshOrderDetailDialog() {
+        final Dialog dialog = new Dialog(OrderActivity.this);
+        dialog.setContentView(R.layout.dialog_refresh_order);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        Button btnOK = dialog.findViewById(R.id.button_ok);
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        Button btnCancel = dialog.findViewById(R.id.button_cancel);
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
+
 }
